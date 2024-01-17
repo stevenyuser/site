2da45fbfdf9069049ac4176818f9f43622d51f9e
Steven Yu <stevenyuser@gmail.com>
2024-01-17 11:30:50 -0500

    updated sitemetadata

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -6,8 +6,8 @@ const siteMetadata = {
   description: 'Steven Yu\'s personal website',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteUrl: 'https://stevenyuser.vercel.app',
+  siteRepo: 'https://github.com/stevenyuser/site',
   siteLogo: '/static/images/logo.png',
   socialBanner: '/static/images/twitter-card.png',
   // mastodon: 'https://mastodon.social/@mastodonuser',
