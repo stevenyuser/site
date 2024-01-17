c639a3d704cd2e150ae0062440c14a426feceb60
Steven Yu <stevenyuser@gmail.com>
2024-01-16 22:54:52 -0500

    initial commit with setup

---
+++ data/headerNavLinks.ts
@@ -0,0 +1,9 @@
+const headerNavLinks = [
+  { href: '/', title: 'Home' },
+  { href: '/blog', title: 'Blog' },
+  { href: '/projects', title: 'Projects' },
+  { href: '/resume.pdf', title: 'Résumé' },
+  { href: '/about', title: 'About' },
+]
+
+export default headerNavLinks
