a935c1e7c6176764772e6da1bbd6eeace134e1e3
Steven Yu <stevenyuser@gmail.com>
2024-01-21 00:45:05 -0500

    updated sitemetadata to remove newsletter

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -20,30 +20,30 @@ const siteMetadata = {
   // threads: 'https://www.threads.net',
   // instagram: 'https://www.instagram.com',
   locale: 'en-US',
-  // analytics: {
-  //   // If you want to use an analytics provider you have to add it to the
-  //   // content security policy in the `next.config.js` file.
-  //   // supports Plausible, Simple Analytics, Umami, Posthog or Google Analytics.
-  //   umamiAnalytics: {
-  //     // We use an env variable for this site to avoid other users cloning our analytics ID
-  //     umamiWebsiteId: process.env.NEXT_UMAMI_ID, // e.g. 123e4567-e89b-12d3-a456-426614174000
-  //   },
-  //   // plausibleAnalytics: {
-  //   //   plausibleDataDomain: '', // e.g. tailwind-nextjs-starter-blog.vercel.app
-  //   // },
-  //   // simpleAnalytics: {},
-  //   // posthogAnalytics: {
-  //   //   posthogProjectApiKey: '', // e.g. 123e4567-e89b-12d3-a456-426614174000
-  //   // },
-  //   // googleAnalytics: {
-  //   //   googleAnalyticsId: '', // e.g. G-XXXXXXX
-  //   // },
-  // },
-  // newsletter: {
-  //   // supports mailchimp, buttondown, convertkit, klaviyo, revue, emailoctopus
-  //   // Please add your .env file and modify it according to your selection
-  //   provider: 'buttondown',
-  // },
+  analytics: {
+    // If you want to use an analytics provider you have to add it to the
+    // content security policy in the `next.config.js` file.
+    // supports Plausible, Simple Analytics, Umami, Posthog or Google Analytics.
+    umamiAnalytics: {
+      // We use an env variable for this site to avoid other users cloning our analytics ID
+      umamiWebsiteId: process.env.NEXT_UMAMI_ID, // e.g. 123e4567-e89b-12d3-a456-426614174000
+    },
+    // plausibleAnalytics: {
+    //   plausibleDataDomain: '', // e.g. tailwind-nextjs-starter-blog.vercel.app
+    // },
+    // simpleAnalytics: {},
+    // posthogAnalytics: {
+    //   posthogProjectApiKey: '', // e.g. 123e4567-e89b-12d3-a456-426614174000
+    // },
+    // googleAnalytics: {
+    //   googleAnalyticsId: '', // e.g. G-XXXXXXX
+    // },
+  },
+  newsletter: {
+    // supports mailchimp, buttondown, convertkit, klaviyo, revue, emailoctopus
+    // Please add your .env file and modify it according to your selection
+    // provider: 'buttondown',
+  },
   // comments: {
   //   // If you want to use an analytics provider you have to add it to the
   //   // content security policy in the `next.config.js` file.
