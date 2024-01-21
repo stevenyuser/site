fe703530d499ae97fa104825ae0546a084ed85e0
Steven Yu <stevenyuser@gmail.com>
2024-01-21 00:16:31 -0500

    moved resume back

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -16,7 +16,7 @@ export default function Projects() {
             Projects
           </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-            Showcase your projects with a hero image (16 x 9)
+            Stuff I've built
           </p>
         </div>
         <div className="container py-12">
